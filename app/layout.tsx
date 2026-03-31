7caebc3a3df76b5833b89271bfd148dccf361eef
Non-territorial <i@nonterritorial.art>
2026-03-31 09:33:11 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Call Center',
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         }}
       >
         {children}
+           <Analytics />
       </body>
     </html>
   )
